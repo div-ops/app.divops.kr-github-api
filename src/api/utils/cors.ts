25ecad0556a72710fee291cec6471a8a9d5cfc0c
정석호 <seokho.jeong@toss.im>
2023-01-01 06:40:41 +0900

    fix [skip vercel] [skip vercel]

--- src/api/utils/cors.ts
+++ src/api/utils/cors.ts
@@ -6,5 +6,6 @@ export async function createCors<RQ, RS>(req: RQ, res: RS) {
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     origin: ["https://www.creco.services", "http://localhost:3000"],
     optionsSuccessStatus: 200,
+    credentials: true,
   });
 }

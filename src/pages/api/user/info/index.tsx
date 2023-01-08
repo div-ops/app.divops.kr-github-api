0a4d2195af1ad139c2270c574f06253ec361d869
정석호 <seokho.jeong@toss.im>
2023-01-08 12:18:44 +0900

    fix(trailing-slash): 지원하도록 추가 [skip vercel]

---
+++ src/pages/api/user/info/index.tsx
@@ -0,0 +1,6 @@
+import { API } from "@divops/github-oauth";
+import { createCors } from "../../../../api/index";
+
+export default API.of({ name: "app-divops-kr" }).UserInfo({
+  before: createCors,
+});

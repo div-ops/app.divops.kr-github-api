8f0cb0153d53dc76395f7c0bc154fddf87eb5b26
정석호 <seokho.jeong@toss.im>
2023-02-19 16:58:59 +0900

    fix: 불필요한 인터페이스 제거 [skip vercel] [skip vercel]

--- src/pages/api/[...GitHubOAuthRoutes].tsx
+++ src/pages/api/[...GitHubOAuthRoutes].tsx
@@ -2,7 +2,6 @@ import { GitHubOAuthRoutes } from "@divops/github-oauth";
 
 export default GitHubOAuthRoutes({
   server: "app-divops-kr",
-  origins: ["https://www.creco.services"],
   prefix: "/api",
   allowedOrigins: {
     "https://www.creco.services": ["memory"],

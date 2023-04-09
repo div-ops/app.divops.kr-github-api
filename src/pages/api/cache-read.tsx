17364e6b993bf0084bb210b2134af07e0c50b497
정석호 <seokho.jeong@toss.im>
2023-04-09 23:31:44 +0900

    fix: more fast [skip vercel]

--- src/pages/api/cache-read.tsx
+++ src/pages/api/cache-read.tsx
@@ -1,29 +1,14 @@
 import fs from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
 
-type Cache = {
-  [key: string]: any;
-};
-
-const cacheFilePath = "/tmp/cache.json";
+const cachePrefix = "/tmp/cache";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (!fs.existsSync(cacheFilePath)) {
-    fs.writeFileSync(cacheFilePath, "{}", "utf-8");
-  }
-
-  if (req.method === "GET" && req.query.key) {
-    const key = req.query.key as string;
-    const cacheFileData = fs.readFileSync(cacheFilePath, "utf8");
-    const cache: Cache = JSON.parse(cacheFileData);
-    const result = cache[key];
-
-    if (result !== undefined) {
-      res.status(200).json({ data: result });
-    } else {
-      res.status(404).json({ error: "Key not found" });
-    }
+  if (req.query.key) {
+    res.status(200).json({
+      data: fs.readFileSync(`${cachePrefix}/${req.query.key}.json`, "utf8"),
+    });
   } else {
-    res.status(400).json({ error: "Bad request" });
+    res.status(400).json({ error: `Bad request (${req.query.key})` });
   }
 }
--- src/pages/api/cache-write.tsx
+++ src/pages/api/cache-write.tsx
@@ -1,28 +1,18 @@
 import fs from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
 
-type Cache = {
-  [key: string]: any;
-};
-
-const cacheFilePath = "/tmp/cache.json";
+const cachePrefix = "/tmp/cache";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (!fs.existsSync(cacheFilePath)) {
-    fs.writeFileSync(cacheFilePath, "{}", "utf-8");
-  }
-
-  if (req.method === "GET" && req.query.key && req.query.data) {
-    const key = req.query.key as string;
-    const data = req.query.data;
-    const cacheFileData = fs.readFileSync(cacheFilePath, "utf8");
-    const cache: Cache = JSON.parse(cacheFileData);
-
-    cache[key] = data;
-    fs.writeFileSync(cacheFilePath, JSON.stringify(cache));
-
-    res.status(200).json({ success: true });
+  if (req.query.key && req.query.data) {
+    res.status(200).json({
+      data: fs.writeFileSync(
+        `${cachePrefix}/${req.query.key}.json`,
+        req.query.data as string,
+        "utf8"
+      ),
+    });
   } else {
-    res.status(400).json({ error: "Bad request" });
+    res.status(400).json({ error: `Bad request (${req.query.key})` });
   }
 }

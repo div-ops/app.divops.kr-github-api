b74015a321e88daefeb0be5912f37c1f9861531c
divopsor <divopsor@gmail.com>
2023-08-09 23:24:12 +0900

    fix(github-api): 로그 추가 [skip vercel]

--- src/pages/api/gist/[category]/create/index.ts
+++ src/pages/api/gist/[category]/create/index.ts
@@ -4,7 +4,12 @@ import { client } from '../../client';
 
 export default async function(req: NextApiRequest, res: NextApiResponse) {
   const [,,,category] = req.url!.split('/');
-  const { contents } = await req.body;
+  const { contents } = req.body;
+
+  const item = {
+    id: generateUUID(),
+    body: contents,
+  };
 
   if (typeof category !== 'string' || !(category in BOX_CATEGORY)) {
     return res.status(400).json({ error: `There is no category of ${category}` });
@@ -13,20 +18,17 @@ export default async function(req: NextApiRequest, res: NextApiResponse) {
   try {
     const data = await client.createItem({
       listId: BOX_CATEGORY[category as keyof typeof BOX_CATEGORY],
-      item: {
-        id: generateUUID(),
-        body: contents,
-      }
+      item,
     });
   
     return res.status(200).json({ data });
   } catch (error: any) {
     if (error.message.includes('찾을 수 없습니다.')) {
-      return res.status(404).json({ message: error.message });
+      return res.status(404).json({ message: error.message, item });
     }
 
     console.error(error.message);
-    return res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message, item });
   }
 }
 

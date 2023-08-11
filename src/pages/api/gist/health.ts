917dc27e7b37a4d5186ae21b9e208d35463b734d
divopsor <divopsor@gmail.com>
2023-08-12 02:38:26 +0900

    fix [skip vercel]

--- src/pages/api/gist/health.ts
+++ src/pages/api/gist/health.ts
@@ -6,10 +6,12 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const [token, id] = req.cookies['Authorization']?.split(':') ?? [];
 
   const secretKey = ensureEnv('GIST_STORAGE_TOKEN') + `:${id}`;
-    
+  console.log({ token, id });
+
   // 암호화된 문자열
   const cryptedToken = pbkdf2Sync(`${id}:${new Date().toISOString().slice(0,7)}`, secretKey, 1000, 64, 'sha512').toString('base64');
 
+  console.log('cryptedToken', cryptedToken.slice(0, 10));
   // 암호화된 문자열이 secret 으로 암호화되었는지 확인
   if (token === cryptedToken) {
     res.status(400).json({ message: 'OK' });

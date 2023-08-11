36d8e92cea66cf83b388b87610e4d5de5a83fe11
divopsor <divopsor@gmail.com>
2023-08-12 01:33:23 +0900

    feat: login and health api [skip vercel]

---
+++ src/pages/api/gist/health.ts
@@ -0,0 +1,19 @@
+import { ensureEnv } from "@divops-packages/node-utils";
+import { NextApiRequest, NextApiResponse } from "next";
+import { pbkdf2Sync } from 'crypto';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const [token, id] = req.headers.authorization?.split?.(':') ?? [];
+  
+  const secretKey = ensureEnv('GIST_STORAGE_TOKEN') + `:${id}`;
+    
+  // 암호화된 문자열
+  const cryptedToken = pbkdf2Sync(`${id}:${new Date().toISOString().slice(0,7)}`, secretKey, 1000, 64, 'sha512').toString('base64');
+
+  // 암호화된 문자열이 secret 으로 암호화되었는지 확인
+  if (token === cryptedToken) {
+    res.status(400).json({ message: 'OK' });
+  } else {
+    res.status(400).json({ message: 'Error' });
+  }
+}
---
+++ src/pages/api/gist/login.ts
@@ -0,0 +1,33 @@
+import { ensureEnv } from "@divops-packages/node-utils";
+import { NextApiRequest, NextApiResponse } from "next";
+import { pbkdf2Sync } from 'crypto';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { id, password } = req.body;
+
+  try {
+    if (ensureEnv('ADMIN_ID') !== id) {
+      return res.status(403).json({
+        message: 'ID 가 올바르지 않습니다.'
+      });
+    }
+
+    if (ensureEnv('ADMIN_PASSWORD') !== password) {
+      return res.status(403).json({
+        message: 'PW 가 올바르지 않습니다.'
+      });
+    }
+
+    const secretKey = ensureEnv('GIST_STORAGE_TOKEN') + `:${id}`;
+    
+    // 암호화된 문자열
+    const cryptedToken = pbkdf2Sync(`${id}:${new Date().toISOString().slice(0,7)}`, secretKey, 1000, 64, 'sha512').toString('base64');
+
+    res.setHeader('Set-Cookie', `Authorization=${cryptedToken}:${id}; path=/; HttpOnly;`);
+    
+    res.status(200).json({ message: 'ok' });
+  } catch (error:any) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+}

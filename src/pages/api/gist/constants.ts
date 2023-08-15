35ca710e6e1e73cef7a498f5cd7d03ba1be72871
divopsor <divopsor@gmail.com>
2023-08-15 21:01:35 +0900

    fix(github-api): private category 3 [skip vercel]

--- src/pages/api/gist/constants.ts
+++ src/pages/api/gist/constants.ts
@@ -1,6 +1,6 @@
 export const BOX_CATEGORY = {
-  work: 'e396a261fcaa122a6544de06f8b74653',
   knowledge: 'a4fd6405fb2a1859662327fcc17511fd',
   remind: 'da4341b2929cef6c316e37261cb20a53',
   diary: '43ccc619a0a2aaecc089da2ac21c1e8c',
+  work: 'e396a261fcaa122a6544de06f8b74653',
 }

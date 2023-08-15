bcafed72fba4eec2e345480f4a06dc611d326e44
divopsor <divopsor@gmail.com>
2023-08-15 21:22:59 +0900

    feat(private): category 가리기 2 [skip vercel]

--- src/pages/api/gist/category.ts
+++ src/pages/api/gist/category.ts
@@ -22,6 +22,6 @@ export default async function(req: NextApiRequest, res: NextApiResponse) {
       }
     }
 
-    return res.status(200).json({ data: BOX_CATEGORY });
+    return res.status(200).json({ data: categories });
   }
 }

d914b6aa4f35c45cc45a45fa32c2dde5e3acc44f
divopsor <divopsor@gmail.com>
2023-08-15 23:58:16 +0900

    fix: add toy [skip vercel]

--- src/pages/api/gist/constants.ts
+++ src/pages/api/gist/constants.ts
@@ -3,6 +3,7 @@ export const BOX_CATEGORY = {
   remind: 'da4341b2929cef6c316e37261cb20a53',
   knowledge: 'a4fd6405fb2a1859662327fcc17511fd',
   diary: '43ccc619a0a2aaecc089da2ac21c1e8c',
+  toy: '0a6f6622eaf56bad5d729dda6d7ba6c5',
 }
 
 export const BOX_CATEGORY_PRIVATE = [

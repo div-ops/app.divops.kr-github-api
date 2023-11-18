9104ef2dcd852bcd5eb283f7d72cf5174c56f123
divopsor <divopsor@gmail.com>
2023-11-19 08:40:09 +0900

    adding: health [skip vercel]

--- src/pages/api/gist/constants.ts
+++ src/pages/api/gist/constants.ts
@@ -1,4 +1,5 @@
 export const BOX_CATEGORY = {
+  health: '703ad4697ea34828468f12e58b78af6b',
   work: 'e396a261fcaa122a6544de06f8b74653',
   remind: 'da4341b2929cef6c316e37261cb20a53',
   knowledge: 'a4fd6405fb2a1859662327fcc17511fd',

0ed5e2ff7d0b33d40229cf654a60b692c8baf037
divopsor <divopsor@gmail.com>
2023-11-25 00:24:59 +0900

    feat(box): private inner-peace 추가 [skip vercel]

--- src/pages/api/gist/constants.ts
+++ src/pages/api/gist/constants.ts
@@ -5,8 +5,10 @@ export const BOX_CATEGORY = {
   knowledge: 'a4fd6405fb2a1859662327fcc17511fd',
   diary: '43ccc619a0a2aaecc089da2ac21c1e8c',
   toy: '0a6f6622eaf56bad5d729dda6d7ba6c5',
+  ['inner-peace']: '86dbd2007960d682fa897adf03ca52bf',
 }
 
 export const BOX_CATEGORY_PRIVATE = [
-  'e396a261fcaa122a6544de06f8b74653' // work
+  'e396a261fcaa122a6544de06f8b74653', // work
+  '86dbd2007960d682fa897adf03ca52bf' // inner-peace
 ];

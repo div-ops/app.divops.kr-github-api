034b9de40006ccb142b5375606ad8dea2d9574c7
divopsor <divopsor@gmail.com>
2024-09-01 02:49:33 +0900

    update [skip vercel]

--- src/pages/api/gist/constants.ts
+++ src/pages/api/gist/constants.ts
@@ -1,17 +1,18 @@
 export const BOX_CATEGORY = {
-  health: '703ad4697ea34828468f12e58b78af6b',
-  work: 'e396a261fcaa122a6544de06f8b74653',
-  remind: 'da4341b2929cef6c316e37261cb20a53',
-  knowledge: 'a4fd6405fb2a1859662327fcc17511fd',
-  diary: '43ccc619a0a2aaecc089da2ac21c1e8c',
-  toy: '0a6f6622eaf56bad5d729dda6d7ba6c5',
-  ['inner-peace']: '86dbd2007960d682fa897adf03ca52bf',
-  ['blog-post']: 'ce62b77189108398c8655c33dbb608ee',
-  ['blog-ps']: '7b5dc7c60bdc631814396fab58d799d9',
-  ['blog-doodle']: '8790e7be4dc84a7e88532302c369efd6',
-}
+  health: "703ad4697ea34828468f12e58b78af6b",
+  work: "e396a261fcaa122a6544de06f8b74653",
+  remind: "da4341b2929cef6c316e37261cb20a53",
+  knowledge: "a4fd6405fb2a1859662327fcc17511fd",
+  diary: "43ccc619a0a2aaecc089da2ac21c1e8c",
+  toy: "0a6f6622eaf56bad5d729dda6d7ba6c5",
+  ["inner-peace"]: "86dbd2007960d682fa897adf03ca52bf",
+  ["blog-post"]: "ce62b77189108398c8655c33dbb608ee",
+  ["blog-ps"]: "7b5dc7c60bdc631814396fab58d799d9",
+  ["blog-doodle"]: "8790e7be4dc84a7e88532302c369efd6",
+  ["config"]: "6b1ffeca3118faa51147f8a19a5c7766",
+};
 
 export const BOX_CATEGORY_PRIVATE = [
-  'e396a261fcaa122a6544de06f8b74653', // work
-  '86dbd2007960d682fa897adf03ca52bf' // inner-peace
+  "e396a261fcaa122a6544de06f8b74653", // work
+  "86dbd2007960d682fa897adf03ca52bf", // inner-peace
 ];
